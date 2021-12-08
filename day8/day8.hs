34d2f431684322fb02353acda22a8ae9b5ece0ee
Adit Chauhan <chauhan.adit.98@gmail.com>
2021-12-08 19:35:51 +0530

    Day 8 Cleaned

--- day8/day8.hs
+++ day8/day8.hs
@@ -4,7 +4,12 @@ import Data.List.Split (splitOn)
 import qualified Data.Map as M
 import Data.Maybe (fromJust)
 import qualified Data.Set as S
-import Debug.Trace
+
+type SetC = S.Set Char
+
+type MIntS = M.Map Int (S.Set Char)
+
+type MSetC = M.Map Char (S.Set Char)
 
 main = do
   inp <- readFile "input"
@@ -13,33 +18,26 @@ main = do
       decodes = map (decode . words) observations
       pairings = zip decodes readings
       part2 (dec, red) = getNumber dec red
-  print $ zip observations readings
   print $ sum (map part2 pairings)
 
 decode obs =
   let nm = numMap obs
-      c5 = common5 obs
-      u5 = uncommon5 obs
-      c6 = common6 obs
-      u6 = uncommon6 obs
+      c5 = common 5 obs
+      u5 = uncommon 5 obs
+      c6 = common 6 obs
+      u6 = uncommon 6 obs
       bindings = get'f nm $ get'c u6 $ get'e u5 nm $ get'b nm $ get'd c5 $ get'g c5 c6 $ get'a nm
    in reMap bindings
 
-common :: [String] -> S.Set Char
-common = foldl (\acc x -> S.intersection acc $ S.fromList x) (S.fromList "abcdefg")
+common n lst = common' $ filter (\x -> length x == n) lst
+  where
+    common' :: [String] -> SetC
+    common' = foldl (\acc x -> S.intersection acc $ S.fromList x) (S.fromList "abcdefg")
 
-uncommon :: [String] -> S.Set Char
-uncommon lst = S.difference (S.fromList "abcdefg") $ common lst
+uncommon :: Int -> [String] -> SetC
+uncommon n lst = S.difference (S.fromList "abcdefg") $ common n lst
 
-uncommon5 lst = S.difference (S.fromList "abcdefg") $ common5 lst
-
-uncommon6 lst = S.difference (S.fromList "abcdefg") $ common6 lst
-
-common5 lst = common $ filter (\x -> length x == 5) lst
-
-common6 lst = common $ filter (\x -> length x == 6) lst
-
-numMap :: [String] -> M.Map Int (S.Set Char)
+numMap :: [String] -> MIntS
 numMap = foldl mapAssigner M.empty
   where
     mapAssigner base x = case length x of
@@ -49,47 +47,43 @@ numMap = foldl mapAssigner M.empty
       7 -> M.insert 8 (S.fromList x) base
       _ -> base
 
-get'a :: M.Map Int (S.Set Char) -> M.Map Char (S.Set Char)
+get'a :: MIntS -> MSetC
 get'a nm =
-  --(trace ("\nnmMap " ++ show nm ++ "\nget'a => 7 : " ++ show (M.lookup 7 nm) ++ " | 1 : " ++ show (M.lookup 1 nm))) $
   toMap M.empty 'a' $ diff (M.lookup 7 nm) (M.lookup 1 nm)
   where
     diff (Just x) (Just y) = S.difference x y
-    diff _ _ = error "get'a None Found"
 
-get'g :: S.Set Char -> S.Set Char -> M.Map Char (S.Set Char) -> M.Map Char (S.Set Char)
+get'g :: SetC -> SetC -> MSetC -> MSetC
 get'g com5 com6 segMap = toMap segMap 'g' $ diff (M.lookup 'a' segMap)
   where
     diff (Just a) = S.difference (S.intersection com5 com6) a
-    diff _ = error "get'g None Found"
 
+get'd :: SetC -> MSetC -> MSetC
 get'd com5 segMap = toMap segMap 'd' $ diff com5 (M.lookup 'a' segMap) (M.lookup 'g' segMap)
   where
     diff com (Just a) (Just g) = S.difference (S.difference com a) g
-    diff _ _ _ = error "get'd None Found"
 
+get'b :: MIntS -> MSetC -> MSetC
 get'b nm segMap = toMap segMap 'b' $ diff (M.lookup 4 nm) (M.lookup 1 nm) (M.lookup 'd' segMap)
   where
     diff (Just four) (Just one) (Just d) = S.difference (S.difference four one) d
-    diff _ _ _ = error "get'b None Found"
 
-get'e :: S.Set Char -> M.Map Int (S.Set Char) -> M.Map Char (S.Set Char) -> M.Map Char (S.Set Char)
+get'e :: SetC -> MIntS -> MSetC -> MSetC
 get'e ucom5 nm segMap = toMap segMap 'e' $ diff ucom5 (M.lookup 1 nm) (M.lookup 'b' segMap)
   where
-    diff :: S.Set Char -> Maybe (S.Set Char) -> Maybe (S.Set Char) -> S.Set Char
     diff ucom (Just one) (Just b) = S.difference (S.difference ucom one) b
-    diff _ _ _ = error "get'e None Found"
 
+get'c :: SetC -> MSetC -> MSetC
 get'c ucom6 segMap = toMap segMap 'c' $ diff ucom6 (M.lookup 'd' segMap) (M.lookup 'e' segMap)
   where
     diff ucom (Just d) (Just e) = S.difference (S.difference ucom d) e
-    diff _ _ _ = error "get'c None Found"
 
+get'f :: MIntS -> MSetC -> MSetC
 get'f nm segMap = toMap segMap 'f' $ diff (M.lookup 1 nm) (M.lookup 'c' segMap)
   where
     diff (Just one) (Just f) = S.difference one f
-    diff _ _ = error "get'f None Found"
 
+reMap :: MSetC -> M.Map SetC Char
 reMap segMap = M.fromList $ zip (map newCode ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]) ['0' .. '9']
   where
     newCode s = foldl (\acc x -> S.union acc (unwrap $ M.lookup x segMap)) S.empty s
